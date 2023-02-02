19355d6b137efe8c65ddc081ced3b0c388839b5e
Fanny Campos <fannycamposa@gmail.com>
2023-02-02 15:25:38 -0300

    GifsService

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
+import { GifsService } from '../services/gifs.service';
+
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
@@ -10,12 +12,15 @@ export class BusquedaComponent {
 
   @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
+  constructor( private gifsService: GifsService ) {}
+
   // Metodo
    buscar(){
 
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
 
+    this.gifsService.buscarGifs( valor );
+    
     this.txtBuscar.nativeElement.value = '';
     
    }
