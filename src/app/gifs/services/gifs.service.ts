2b7a1b27feb56979d7db914d9b7ea31dea4d1d54
Fanny Campos <fannycamposa@gmail.com>
2023-02-09 13:59:15 -0300

    Mostrar los resultados en pantalla

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,6 +9,9 @@ export class GifsService {
   private apiKey : string = '02n1wuHOZRCLS3jKlx4tZ01bQcgqJQyy';
   private _historial: string[] = [];
 
+  // TODO: Cambiar any por su tipo
+  public resultados: any[] = [];
+
   get historial() {
     return [...this._historial];
   }
@@ -24,10 +27,11 @@ export class GifsService {
        this._historial = this._historial.slice(0,10);
     }
 
-    this.http.get( 'https://api.giphy.com/v1/gifs/search?api_key=02n1wuHOZRCLS3jKlx4tZ01bQcgqJQyy&q=dragon ball z&limit=10' )
+    this.http.get( `https://api.giphy.com/v1/gifs/search?api_key=02n1wuHOZRCLS3jKlx4tZ01bQcgqJQyy&q=${ query } z&limit=10` )
           .subscribe( ( resp: any ) => {
             console.log( resp.data );
-          })
+            this.resultados = resp.data;
+          });
 
 
   }
