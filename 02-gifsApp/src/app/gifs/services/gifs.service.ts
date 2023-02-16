f414902f96a330eb1c97649d1b264b9df9c302dc
fanny00 <fannycamposa@gmail.com>
2023-02-16 17:57:46 -0300

    LocalStorage

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -17,7 +17,15 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor( private http: HttpClient ){};
+  constructor( private http: HttpClient ){
+
+    this._historial = JSON.parse( localStorage.getItem('historial')! ) || [];
+
+    // if( localStorage.getItem('historial') ){
+    //    this._historial = JSON.parse( localStorage.getItem('historial')!);
+    // }
+
+  };
 
   buscarGifs( query:string ) {
 
@@ -26,6 +34,8 @@ export class GifsService {
     if( !this._historial.includes( query ) ){
        this._historial.unshift( query );
        this._historial = this._historial.slice(0,10);
+
+       localStorage.setItem( 'historial', JSON.stringify( this._historial ) );
     }
 
     this.http.get<SearchGifsResponse>( `https://api.giphy.com/v1/gifs/search?api_key=02n1wuHOZRCLS3jKlx4tZ01bQcgqJQyy&q=${ query } z&limit=10` )
